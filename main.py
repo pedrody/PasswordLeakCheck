43d1e7adb9ecce633a1ea34c85f185362e3fec43
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2024-02-08 22:55:08 -0300

    feat: add "check_password" function to check if password has been compromised

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 import hashlib
 
 
@@ -17,6 +18,34 @@ def convert_password_to_hash(password):
     prefix, suffix = sha1_password[:5], sha1_password[5:]
 
     return prefix, suffix
+
+
+def check_password(password):
+    """
+    The `check_password` function checks if a given password has been compromised by
+    querying the Pwned Passwords API.
+    
+    :param password: The `password` parameter is a string that represents the
+    password that needs to be checked
+    :return: The function `check_password` returns an integer value representing the
+    number of times the given password has been found in breached password
+    databases.
+    """
+    prefix, suffix = convert_password_to_hash(password)
+
+    url = f'https://api.pwnedpasswords.com/range/{prefix}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        hashes = (line.split(':') for line in response.text.splitlines())
+        
+        for h, count in hashes:
+            if h == suffix:
+                return int(count)
+        return 0
+    
+    else:
+        response.raise_for_status()
 
 if __name__ == '__main__':
     ...
